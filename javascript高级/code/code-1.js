af8b5fe0714e636813b23bfd4dda0167dad3c693
lzhan <hzy_zl@163.com>
2018-12-26 17:13:43 +0800

    2018-12-27 lzhan

--- javascript高级/code/code-1.js
+++ javascript高级/code/code-1.js
@@ -4,32 +4,79 @@
 // 'use strict';
 
 
-
-
-var a = 1;
-function foo() {
-    a = 10;
-    return;
-    var a=function () {}
-}
-foo();
-console.log(a);
+// var a = 1;
+// function foo() {
+//     a = 10;
+//     return;
+//     var a=function () {}
+// }
+// foo();
+// console.log(a);
 
 
 //等价于...
-var a;
+// var a;
+//
+// function foo() {
+//     var a;
+//     a=function () {
+//
+//     };
+//     a=10;
+//     return;
+// }
+//
+// a=1;
+//
+// foo();
+// console.log(a);
 
-function foo() {
-    var a;
-    a=function () {
+'use strict';
 
-    };
-    a=10;
-    return;
+
+// function foo() {
+//     var arr=new Array(10);
+//     var i=0;
+//     for(;i<arr.length;i++){ //i 为foo 变量对象属性
+//         // arr[i]=function () {
+//         //         // arr[i] 都保存一个匿名方法的引用
+//         //     return i;   //每个匿名方法作用域链都指向foo 变量对象
+//         // }
+//
+//         // (function (a) {
+//         //     arr[a]=a;
+//         // })(i)
+//
+//         function med(a) {
+//             arr[a]=a;
+//         }
+//
+//         med(i);
+//     }
+//     return arr;
+// }
+// var v=foo()[8];
+// console.log(v);
+
+
+var user={
+    name:'tom',
+    hobby:['read','sleep']
 }
 
-a=1;
+// user.age=12;
+//
+// delete user.age;
 
-foo();
-console.log(a);
+var hob=user.hobby;
+
+
+
+
+console.log(user.hobby);
+
+console.log(hob);
+
+
+var n=new Number(12);
 
